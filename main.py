ef02fa744fd478849bceffc8a8a2fe726ed8ac57
Klay Clarke <klayaclarke@gmail.com>
2021-11-23 18:32:26 -0500

    create save method that saves user input to a txt file - method runs when user clicks 'add' button

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from tkinter import *
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate():
+    pass
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website_text = website_input.get()
+    email_text = email_input.get()
+    password_text = password_input.get()
+    with open('data.txt', mode='w') as file:
+        file.writelines(f'{website_text} | {email_text} | {password_text}')
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -14,25 +25,36 @@ logo_image = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=logo_image)
 canvas.grid(column=1, row=0)
 
-wesbite_label = Label(text='Website')
-wesbite_label.grid(column=0, row=1)
+# Labels
+
+website_label = Label(text='Website:')
+website_label.grid(column=0, row=1)
+
+email_label = Label(text='Email/Username:')
+email_label.grid(column=0, row=2)
+
+password_label = Label(text='Password:')
+password_label.grid(column=0, row=3)
+
+# Text Boxes
+
 website_input = Entry(width=35)
+website_input.focus()
 website_input.grid(column=1, row=1, columnspan=2)
 
-email_label = Label(text='Email/Username')
-email_label.grid(column=0, row=2)
 email_input = Entry(width=35)
+# email_input.insert() --> insert commonly used email to pre-populate the text field whenever the app is running
 email_input.grid(column=1, row=2, columnspan=2)
 
-password_label = Label(text='Password')
-password_label.grid(column=0, row=3)
 password_input = Entry(width=21)
 password_input.grid(column=1, row=3)
 
-generate_button = Button(text='Generate')
+# Buttons
+
+generate_button = Button(text='Generate Password', command=generate)
 generate_button.grid(column=2, row=3)
 
-add_button = Button(text='Add', width=36)
+add_button = Button(text='Add', width=36, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
