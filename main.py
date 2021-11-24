7201c6ae1d0939606b78594dc1243fa655671497
Klay Clarke <klayaclarke@gmail.com>
2021-11-23 19:17:50 -0500

    if user presses 'add' with at least one text field empty, user will receive a warning stating the mistake.
    user is unable to submit until all fields are populated

--- main.py
+++ main.py
@@ -12,16 +12,20 @@ def save():
     website_text = website_input.get()
     email_text = email_input.get()
     password_text = password_input.get()
+    if website_text == '' or email_text == '' or password_text == '':
+        messagebox.showwarning(message='Please do not leave any of the fields empty!')
 
-    is_ok = messagebox.askokcancel(title=website_text, message=f'These are the details entered: \nEmail: {email_text} '
-                                                       f'\nPassword: {password_text} \nIs it ok to save?')
+    else:
+        is_ok = messagebox.askokcancel(title=website_text,
+                                       message=f'These are the details entered: \nEmail: {email_text} '
+                                               f'\nPassword: {password_text} \nIs it ok to save?')
 
-    if is_ok:
-        with open('data.txt', mode='a') as file:
-            file.writelines(f'{website_text} | {email_text} | {password_text}\n')
-        website_input.delete(0, END)
-        password_input.delete(0, END)
-        website_input.focus()
+        if is_ok:
+            with open('data.txt', mode='a') as file:
+                file.writelines(f'{website_text} | {email_text} | {password_text}\n')
+            website_input.delete(0, END)
+            password_input.delete(0, END)
+            website_input.focus()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
